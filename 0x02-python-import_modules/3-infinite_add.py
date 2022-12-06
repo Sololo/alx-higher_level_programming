703fbb89c187f3cac07d7dd900a0512da908e1ca
Sololo <philasololo@gmail.com>
2022-12-06 01:04:50 -0800

    fix

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,9 +3,9 @@ import sys
 
 
 def main(*argv):
-    l = len(sys.argv)
+    length = len(sys.argv)
     sum = 0
-    if l > 1:
+    if length > 1:
         for args in sys.argv:
             if args != sys.argv[0]:
                 sum = sum + int(args)
