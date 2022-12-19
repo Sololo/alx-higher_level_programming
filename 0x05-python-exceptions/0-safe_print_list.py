6e609547b3351e05fddc8bebf2c1e2a5cf4c4f78
Sololo <philasololo@gmail.com>
2022-12-19 16:19:17 +0200

    fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-"""
-prints x elements of a list.
-"""
-
-
 def safe_print_list(my_list=[], x=0):
-    nb = 0
-    for x in range(0, x):
-        try:
-            print(my_list[x], end="")
-            nb += 1
-        except:
-            break
+    y = 0
+    if not my_list:
         print("")
-        return (nb)
+        return (0)
+    while y < x:
+        try:
+            print("{}".format(my_list[y]), end="")
+            y = y + 1
+        except IndexError:
+            break
+    print("")
+    return (y)
--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """
 Prints an integer with "{:d}".format().
 """
