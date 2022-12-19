54b554dd4c0f468fb89a9ffc83100a2f33dfbd5a
Sololo <philasololo@gmail.com>
2022-12-19 05:15:31 -0800

    fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,6 +3,7 @@
 prints x elements of a list.
 """
 
+
 def safe_print_list(my_list=[], x=0):
     nb = 0
     for x in range(0, x):
@@ -12,4 +13,4 @@ def safe_print_list(my_list=[], x=0):
         except:
             break
         print("")
-        return(nb)
+        return nb
