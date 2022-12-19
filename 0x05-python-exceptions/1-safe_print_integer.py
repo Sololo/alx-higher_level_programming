9635fcb3a4c485b9815bf27fa6a604fcb562fa54
Sololo <philasololo@gmail.com>
2022-12-19 06:02:14 -0800

    fix

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -6,6 +6,6 @@ Prints an integer with "{:d}".format().
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-        return(True)
+        return True
     except:
         return False
