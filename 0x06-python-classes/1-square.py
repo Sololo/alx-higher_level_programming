8abbeb9960d5a1c0f2c8b9cf8a31a9140fbd54d6
Sololo <philasololo@gmail.com>
2022-12-20 16:20:51 +0200

    Update

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Module Sqaure """
+
+
+class Square:
+    """ 
+    Square class defined by geometric shap
+    """
+    def __init__(self, size):
+        """
+        Initialize methode
+        Args:
+            size (int): size of square
+        """
+        self.__size = size
